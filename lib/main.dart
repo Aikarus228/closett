5076bed874bdc77508bf87cf1e019faec549499f
Aikarus228 <matgvo123@gmail.com>
2023-11-13 23:11:57 +0300

    first

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ClosetWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class ClosetWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 3,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 3,
+                child: Container(
+                  color: const Color.fromARGB(255, 77, 249, 255),
+                  margin: EdgeInsets.all(10.0),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Color.fromARGB(255, 216, 210, 120),
+                  margin: EdgeInsets.all(10.0),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: const Color.fromARGB(255, 58, 183, 146),
+                  margin: EdgeInsets.all(10.0),
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Container(
+                  color: const Color.fromARGB(255, 255, 59, 59),
+                  margin: EdgeInsets.all(10.0),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 3,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 3,
+                child: Container(
+                  color: const Color.fromARGB(255, 238, 240, 105),
+                  margin: EdgeInsets.all(10.0),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: const Color.fromARGB(255, 39, 24, 4),
+                  margin: EdgeInsets.all(10.0),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: const Color.fromARGB(255, 84, 96, 102),
+                  margin: EdgeInsets.all(10.0),
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Container(
+                  color: const Color.fromARGB(255, 68, 255, 109),
+                  margin: EdgeInsets.all(10.0),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
